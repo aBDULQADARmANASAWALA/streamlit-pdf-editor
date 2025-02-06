4cf4d34b1a888235aa230f3253ef18f2150a3ac5
Abdulqadar Manasawala <72400908+aBDULQADARmANASAWALA@users.noreply.github.com>
2025-02-06 17:19:31 +0000

    trying to fix bugs 002

--- streamlit_pdf_editor.py
+++ streamlit_pdf_editor.py
@@ -10,7 +10,10 @@ if uploadedfile is not None:
     reader_pg = PdfReader(uploadedfile).pages[0]
     writer = PdfWriter(clone_from="stamp.pdf")
 
-    writer.pages[0].merge_page(reader_pg, expand=True)
+    try:
+        writer.pages[0].merge_page(reader_pg, over=False)
+    except Exception as e:
+        st.error(f"An error has occured")
 
     writer.write(f"{name}_e.pdf")
     with open(f"{name}_e.pdf", "rb") as pdf:
